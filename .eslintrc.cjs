e6c4f64e983bc685f8f03c9dc3120bb47bc16965
loxygenk <me@loxygen.dev>
2023-04-02 17:03:56 +0900

    🔧 Configure ESLint, Prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier"
+  ],
+  overrides: [
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: [
+    "react",
+    "@typescript-eslint"
+  ],
+  rules: {
+    // importing React in jsx is no longer neccesary with React 17 or later
+    "react/react-in-jsx-scope": "off"
+  },
+  settings: {
+    react: {
+      version: "detect"
+    }
+  }
+}
